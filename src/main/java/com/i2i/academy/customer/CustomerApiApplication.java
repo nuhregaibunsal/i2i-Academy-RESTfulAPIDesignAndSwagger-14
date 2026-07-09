f7aa3658b6da62371ba9f76a5d5e661f622e5c40
nuhregaibunsal <nuhregaibunsal4114@gmail.com>
2026-07-09 10:19:39 +0300

    chore: bootstrap Spring Boot project with Oracle datasource config

---
+++ src/main/java/com/i2i/academy/customer/CustomerApiApplication.java
@@ -0,0 +1,12 @@
+package com.i2i.academy.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApiApplication.class, args);
+    }
+}
